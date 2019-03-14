99d82dcf8bbe616b9a2be3985e0cb6aa88eb8d9e
Yong Jun Jung <keyos27@gmail.com>
2019-03-13 18:04:07 -0700

    aa

---
+++ master-file/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Amazing Store by Amazing Jun, Amazing Marco, Amazing Lorenzo, Amazing Tolik</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!--------------------------------------------------------------------------------
+   _                       _               __ _                   _                                       _                                      _      
+  /_\  _ __ ___   __ _ ___(_)_ __   __ _  / _\ |_ ___  _ __ ___  | |__  _   _    __ _ _ __ ___   __ _ ___(_)_ __   __ _   _ __   ___  ___  _ __ | | ___ 
+ //_\\| '_ ` _ \ / _` |_  / | '_ \ / _` | \ \| __/ _ \| '__/ _ \ | '_ \| | | |  / _` | '_ ` _ \ / _` |_  / | '_ \ / _` | | '_ \ / _ \/ _ \| '_ \| |/ _ \
+/  _  \ | | | | | (_| |/ /| | | | | (_| | _\ \ || (_) | | |  __/ | |_) | |_| | | (_| | | | | | | (_| |/ /| | | | | (_| | | |_) |  __/ (_) | |_) | |  __/
+\_/ \_/_| |_| |_|\__,_/___|_|_| |_|\__, | \__/\__\___/|_|  \___| |_.__/ \__, |  \__,_|_| |_| |_|\__,_/___|_|_| |_|\__, | | .__/ \___|\___/| .__/|_|\___|
+                                   |___/                                |___/                                     |___/  |_|              |_|           
+	------------------------------------------------------------------------------------>
+
+		<!-- Font Import CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<!-- Main CSS -->
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="hero-wrap">
+			<!-- Start of Nav -->
+			<?php include('')?>
+			<nav id="navbar">
+				<img src="images/logo_1.svg" alt="Amazing Store" class="logo">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Shop</a><i class="fas fa-angle-down"></i> 
+						<ul>
+							<li><a href="#">Item 1</a></li>
+							<li><a href="#">Item 2</a></li>
+							<li><a href="#">Item 3</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Cart</a><i class="fas fa-angle-down"></i> 
+						<ul>
+							<li><a href="#">Check Out</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contact</a></li>
+					<li class="lastLi">
+						<form>
+							<input type="text" placeholder="Search.." name="search">
+							<button type="submit"><i class="fa fa-search"></i></button>
+						</form>
+					</li>
+				</ul>
+			</nav>
+			<!-- End of Nav -->
+
+			<!-- Start of Homepage -->
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+					<h3 class="v">Collectible Relics Since - 1985 relics</h3>
+					<h3 class="vr">Copyright 2019 Amazing Store. All Rights Reserved.</h3>
+					<div class="col-md-11 ftco-animate text-center">
+						<h1 id="homeHeader">L'incroyable Magasin</h1>
+						<h2 id="homeSub"><span>Embrace Your Art</span></h2>
+					</div>
+					<div class="mouse">
+						<a href="#" class="mouse-icon">
+							<div class="mouse-wheel"><span class="ion-ios-arrow-down"></span></div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<!-- End of Homepage -->
+		</div>
+
+		<script src="js/jquery.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
